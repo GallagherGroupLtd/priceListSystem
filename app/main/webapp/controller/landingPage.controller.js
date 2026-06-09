a93a638ae72cafc69ea4cf3f3e04c810e5017f90
Rohit Singhal <rohit.singhal@gallagher.com>
2026-06-09 06:10:49 +0000

    Fix for 4 issues on Landing page (Title, Sub-Title, Image and Contact Information - Email and Phone Number)

--- app/main/webapp/controller/landingPage.controller.js
+++ app/main/webapp/controller/landingPage.controller.js
@@ -14,7 +14,8 @@ sap.ui.define([
             //Create a JSON Model to hold display data.
             var oViewModel = new JSONModel({
                 currentDate: new Date(),
-                userName: "" // Default placeholder
+                userName: "", // Default placeholder
+                userEmail: ""   //Default placeholder for email.
             });
 
             //Set the model to the view with the name "home".
@@ -26,8 +27,14 @@ sap.ui.define([
             //Fetch real user name if running in Fiori Launchpad.
             this._setUserName(oViewModel);
 
+            //Fetch logged in user email if running in Fiori Launchpad.
+            this._setUserEmail(oViewModel);
+
             //Set Picture using UI module path to ensure it works in both local and Fiori environments.
             this._setImagePath();
+
+            //Function to set models for view, to allow information on the Landing page to be user-specific and dynamic.
+            this._setDataModelsForView();
         },
 
         _setFormattedDate: function (oModel) {
@@ -53,6 +60,99 @@ sap.ui.define([
             const sRootPath = sap.ui.require.toUrl("pricelistapp/main");
             const oImageModel = new sap.ui.model.json.JSONModel({ banner: sRootPath + "/images/TrendsBanenrBG.jpg" });
             this.getView().setModel(oImageModel, "imgModel");
+        },
+
+        // New method to set user email
+        _setUserEmail: function (oModel) {
+            if (sap.ushell && sap.ushell.Container) {
+                var oUser = sap.ushell.Container.getUser();
+                var sEmail = oUser.getEmail();
+                oModel.setProperty("/userEmail", sEmail);
+            }
+        },
+
+        //Function to set up any additional models needed for the view, such as user-specific data or dynamic content.
+        _setDataModelsForView: function () {
+            //Fetching email from json model home
+            const userEmail = this.getView().getModel("home").getProperty("/userEmail");
+            //Fetching the reference of the oData model defined in manifest.json file with the name mainService
+            const mainModel = this.getView().getModel("mainService");
+
+            //using email as a filter, reading entity: AccountAssignment, to fetch user commercial scope
+            //Added select query to fetch specific details required for further use.
+            const srcPath = "/AccountAssignment?$filter=Email eq '" + userEmail + "'&$select=CustomerNumber,Email,FirstName,HasActiveEntity,HasDraftEntity,ID,IsActiveEntity,LastName,MarketScopeCountry,MarketScopeRegion,TradeScenario,SalesOrg";
+
+            mainModel.read(srcPath, {
+                success: function (oData) {
+                    if (oData && oData.value && oData.value.length > 0) {
+                        //GUID of the user, stored in account assignment table.
+                        const userGUID = oData.value[0].ID;
+                        const MarketScopeCountry = oData.value[0].MarketScopeCountry;
+                        const MarketScopeRegion = oData.value[0].MarketScopeRegion;
+                        const TradeScenario = oData.value[0].TradeScenario;
+                        const SalesOrg = oData.value[0].SalesOrg;
+
+                        //Storing the user GUID in a new JSON model named userModel for use across the application.
+                        const oUserModel = new JSONModel({
+                            userGUID: userGUID,
+                            MarketScopeCountry: MarketScopeCountry,
+                            MarketScopeRegion: MarketScopeRegion,
+                            TradeScenario: TradeScenario,
+                            SalesOrg: SalesOrg
+                        });
+                        this.getView().setModel(oUserModel, "userModel");
+
+                        //Path to fetch tile Content which will be used to show image, title and sub-title on the landing page.
+                        const tileContentPath = "/TileContent?$filter=MarketScopeCountry eq '" + MarketScopeCountry + "' and MarketScopeRegion eq '" + MarketScopeRegion + "' and TradeScenario eq '" + TradeScenario + "'&$select=HasActiveEntity,HasDraftEntity,ID,ImageLink,InformationDetails,InformationHeading,IsActiveEntity,MarketScopeCountry,MarketScopeRegion,TradeScenario";
+                        //Read call to fetch tile content based on user's market scope and trade scenario, and set it to a model named tileModel for binding in the view.
+                        mainModel.read(tileContentPath, {
+                            success: function (oTileData) {
+                                if (oTileData && oTileData.value && oTileData.value.length > 0) {
+                                    const oTileModel = new JSONModel({
+                                        heading: oTileData.value[0].InformationHeading,
+                                        subHeading: oTileData.value[0].InformationDetails,
+                                        image: oTileData.value[0].ImageLink
+                                    });
+                                    this.getView().setModel(oTileModel, "tileModel");
+
+                                    this.getView().getModel("imgModel").setProperty("/banner", oTileData.value[0].ImageLink);
+                                } else {
+                                    MessageToast.show("No tile content found for the user's market scope.");
+                                }
+                            }.bind(this),
+                            error: function (oError) {
+                                MessageToast.show("Error fetching tile content: " + oError.message);
+                            }
+                        });
+
+                        //Path to fetch contact information of the user based on market scope and trade scenario, which will be displayed on the landing page.
+                        const contactInfoPath = "/ContactInfo?$filter=MarketScopeCountry eq '" + MarketScopeCountry + "' and MarketScopeRegion eq '" + MarketScopeRegion + "' and TradeScenario eq '" + TradeScenario + "'&$select=ContactEmail,ContactNumber,ExternalAccount,HasActiveEntity,HasDraftEntity,ID,InternalAccount,IsActiveEntity";
+                        //Read call to fetch contact information and set it to a model named contactModel for binding in the view.
+                        mainModel.read(contactInfoPath, {
+                            success: function (oContactData) {
+                                if (oContactData && oContactData.value && oContactData.value.length > 0) {
+                                    const oContactModel = new JSONModel({
+                                        contactEmail: oContactData.value[0].ContactEmail,
+                                        contactNumber: oContactData.value[0].ContactNumber
+                                    });
+                                    this.getView().setModel(oContactModel, "contactModel");
+                                } else {
+                                    MessageToast.show("No contact information found for the user's market scope.");
+                                }
+                            }.bind(this),
+                            error: function (oError) {
+                                MessageToast.show("Error fetching contact information: " + oError.message);
+                            }
+                        });
+                    } else {
+                        MessageToast.show("No account assignment found for the user.");
+                    }
+                }.bind(this),
+                error: function (oError) {
+                    MessageToast.show("Error fetching account assignment: " + oError.message);
+                }
+            });
+            
         },
 
         onNavigationAppPress: function (oEvent) {
